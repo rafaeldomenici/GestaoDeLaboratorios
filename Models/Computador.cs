af81734859009b42e622cfd3f4dde54cab1d9fc8
Rafael Domenici <rafaeldomenici@hotmail.com>
2024-03-18 16:40:44 -0300

    adicionando projeto

---
+++ Models/Computador.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestaoDeLaboratorios.Models
+{
+    public class Computador
+    {
+        [Key]
+        private int id;
+        private string marca;
+        private string processador;
+        private string placaMae;
+        private string memoria;
+        private string hd;
+        private int numeroPatrimonio;
+        private DateTime dataCompra;
+
+        public int Id { get => id; set => id = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public string Processador { get => processador; set => processador = value; }
+        public string PlacaMae { get => placaMae; set => placaMae = value; }
+        public string Memoria { get => memoria; set => memoria = value; }
+        public string Hd { get => hd; set => hd = value; }
+        public int NumeroPatrimonio { get => numeroPatrimonio; set => numeroPatrimonio = value; }
+        public DateTime DataCompra { get => dataCompra; set => dataCompra = value; }
+    }
+}
